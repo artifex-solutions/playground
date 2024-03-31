c55893d1e9bfad12e08c10fe0b309ac2d3c0bc53
magbor <magbor@rm.dk>
2024-04-01 00:08:07 +0200

    update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,32 @@
 module.exports = {
-  root: true,
-  extends: ["@nuxt/eslint-config"],
+	root: true,
+	extends: [
+		'@nuxt/eslint-config',
+		'plugin:prettier/recommended', // should be last - enables both prettier plugin and enables eslint-config-prettier
+	],
+
+	plugins: ['@stylistic'],
+
+	rules: {
+		'prettier/prettier': 1, // Prettier rules are set to warnings but will be autofixed
+		'no-console': ['warn', { allow: ['warn', 'error'] }],
+		camelcase: [
+			'warn',
+			{
+				allow: [
+					/* add non-camelCased variables that are valid here */
+				],
+			},
+		],
+		'@stylistic/object-property-newline': [
+			'error',
+			{
+				allowAllPropertiesOnSameLine: true,
+			},
+		],
+		// Vue
+		'vue/no-multiple-template-root': 'off',
+		'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+		'vue/multi-word-component-names': 'off',
+	},
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,17 @@ export default defineNuxtConfig({
     jwt: {
       secretKey: "Cp3ehREayzaa1OimUrpSRRbaWrVtOk5MBuZOhbJPCFt-xGuG_DG4CvKL_iUz50lB"
     }
-  }
+  },
+
+  typescript: {
+		strict: true,
+	},
+
+	$development: {
+		sourcemap: true,
+	},
+
+	experimental: {
+		typedPages: true,
+	},
 })
